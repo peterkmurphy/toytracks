fb1084c01219f6d39d97b310ad73904561896ddf
Peter Murphy <peterkmurphy@gmail.com>
2018-01-22 23:57:17 +1000

    Getting Postgres converted

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20180121052729) do
 
-  create_table "items", force: true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.binary   "photo"
@@ -25,9 +28,9 @@ ActiveRecord::Schema.define(version: 20180121052729) do
     t.datetime "updated_at"
   end
 
-  add_index "items", ["user_id"], name: "index_items_on_user_id"
+  add_index "items", ["user_id"], name: "index_items_on_user_id", using: :btree
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -42,7 +45,7 @@ ActiveRecord::Schema.define(version: 20180121052729) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
